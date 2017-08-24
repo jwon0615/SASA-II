385313fa181cedf800ba1ac0d94307264aa5b627
jwon0615 <31298001+jwon0615@users.noreply.github.com>
2017-08-24 11:17:36 +0900

    1~9,999,999까지의 합

--- 1506 박지원/test/test/main.cpp
+++ 1506 박지원/test/test/main.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!\n");
+    long long int a=0;
+    for(int i=1;i<=9999999;i++)
+    {
+        a+=i;
+    }
+    printf("%lld",a);
     return 0;
 }
