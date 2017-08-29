30582c0b843ceab6636b9f8a834bd88aa1a7d0bc
jwon0615 <31298001+jwon0615@users.noreply.github.com>
2017-08-29 10:29:24 +0900

    .
    .

--- run/run/main.cpp
+++ run/run/main.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <algorithm>
 
-int n, arr[101];
+int n, k, a[100];
+
+
+int find(int s, int e, int k) //시작값, 종단값, 탐색값
+{
+    while(s<=e)
+    {
+        int m=(s+e)/2;
+        if(a[m]==k) return m;
+        else if(a[m]>k) e=m-1;
+        else s=m+1;
+    }
+    return -1;
+}
 
 int main()
 {
-    
     scanf("%d",&n);
-    
     for(int i=0; i<n; i++)
-        scanf("%d",&arr[i]);
-    
-    std::sort(arr,arr+n-1);
-    
-    for(int i=0; i<n; i++)
-        printf("%d ",arr[i]);
-    
-    return 0;
+        scanf("%d",a+i);
+    scanf("%d", &k);
+    printf("%d ",find(0,n-1,k)+1);
+        return 0;
 }
