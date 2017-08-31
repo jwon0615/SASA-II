5360766a14e764f7ea126298bf0302a398864362
jwon0615 <31298001+jwon0615@users.noreply.github.com>
2017-08-31 11:30:48 +0900

    김정화쌤 1차시
    1부터n까지의 합
    k진법 출력

--- 1506 박지원/김정화쌤/1부터n까지의 합.cpp
+++ 1506 박지원/김정화쌤/1부터n까지의 합.cpp
@@ -1,21 +1,26 @@
 //수학적 귀납법
-//10진법 정수 n을 k 진법으로 출력하기
+//1부터 n까지의 합 구하기
 
 #include <stdio.h>
 #include <time.h> //cplusplus.com
 
-void f(int n, int k){
-    if(n<k) printf("%d",n);
-    else f(n/k,k), printf("%d",n%k);
+//long long int f(long long int n){
+//    if(n==1) return 1;
+//    return f(n-1)+n;
+//}
+
+long long int f(long long int n){
+    if(n==1) return 1;
+    return 2*f(n/2)+((n+1)/2)*((n+1)/2);
 }
 
 int main(){
-    int n, k;
-    scanf("%d %d",&n, &k);
+    int n;
+    scanf("%d",&n);
     
     time_t s=clock();    //실행 시간 측정 시작
     
-    f(n,k);
+    printf("%lld\n",f(n));
     
     time_t e=clock();
     printf("\n%f seconds\n",(float)(e-s)/CLOCKS_PER_SEC); //실행 시간 측정 완료
