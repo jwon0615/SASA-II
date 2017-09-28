a2b10d5797fb1afa3cc56e1d75142caf59ed3cc1
jwon0615 <jwon0615@sasa.hs.kr>
2017-09-28 10:59:19 +0900

    commiting
    commiting

--- run/run/main.cpp
+++ run/run/main.cpp
@@ -1,29 +1,35 @@
-//영역 나누기 (파일입출력)
+#include <stdio.h>
 
-#include <cstdio>
+int n, k, a[10000], b[10000], arr[500000];
 
-int P[1<<7][1<<7], n;
-
-bool isOne(int a, int b, int s, int v){
-    for(int i=a; i<a+s; i++)
-        for(int j=b; j<b+s; j++)
-            if(P[i][j]!=v) return 0;
-    return 1;
+int lb(int s, int e, int k){ //시작값, 종단값, 탐색값
+    while(s<e){
+        int m=(s+e)/2;
+        if(arr[m]>=k) e=m;
+        else s=m+1;
+    }
+    return e+1;
 }
 
-int f(int a, int b, int s, int v){
-    if(s==1) return P[a][b]==v;
-    if(isOne(a,b,s,v)) return 1;
-    return f(a,b,s/2,v)+f(a+s/2,b,s/2,v)+f(a,b+s/2,s/2,v)+f(a+s/2,b+s/2,s/2,v);
+int ub(int s, int e, int k){ //시작값, 종단값, 탐색값
+    while(s<e){
+        int m=(s+e)/2;
+        if(arr[m]>k) e=m;
+        else s=m+1;
+    }
+    return e+1;
 }
 
 int main(){
-    FILE *in = fopen("input.txt","r");
-    FILE *out = fopen("output.txt","w");
+    //입력
+    scanf("%d", &n); //첫 줄에 배열의 길이 n (2 ≤ n ≤ 500,000)
+    for(int i=0; i<n; i++) //두 번째 줄부터 n개의 수 (0 ≤ 수 ≤ 500,000)
+        scanf("%d", arr+i);
+    scanf("%d", &k); //세 번째 줄에 두 수가 입력되는 횟수 z (1 ≤ k ≤ 10,000)
+    for(int i=0; i<k; i++) //네 번째 줄부터 k줄에 걸쳐 두 수 a, b (-1,000,000 ≤ a ≤ b ≤ 1,000,000)
+        scanf("%d %d", a+i, b+i);
     
-    fscanf(in,"%d",&n);
-    for(int i=0; i<n; i++)
-        for(int j=0; j<n; j++)
-            fscanf(in, "%d", &P[i][j]);
-    fprintf(out, "%d\n%d\n", f(0,0,n,0), f(0,0,n,1));
+    //출력
+    for(int i=0; i<k; i++) //k줄에 걸쳐 입력받은 두 수 사이(두 수 포함)에 있는 숫자의 갯수
+       printf("%d\n", ub(0,n,b[i])-lb(0,n,a[i]));
 }
